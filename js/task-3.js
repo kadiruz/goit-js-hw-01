2c6574758880af328a58cd11fc9e09d19daae347
kadir <kdruz36@gmail.com>
2024-12-31 19:02:43 +0300

    first

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+    let boxSizing = (Number.parseFloat(content) + 2*Number.parseFloat(padding) + 2*Number.parseFloat(border));
+   return boxSizing;
+ }
+ 
+ console.log(getElementWidth("50px", "8px", "4px")); 
+ console.log(getElementWidth("60px", "12px", "8.5px")); 
+ console.log(getElementWidth("200px", "0px", "0px")); 
+ 
